14ad3830cf1358df2927e347fc0aeb52b2639216
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-09 10:24:03 -0600

    Componente personalizado PrimaryButton

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,20 +1,17 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {PrimaryButton} from '../components';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(1);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
-      <Pressable
+      <PrimaryButton
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-        style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
-        <Text style={{color: Platform.OS === 'android' ? 'white' : '#4746AB'}}>
-          Incrementar
-        </Text>
-      </Pressable>
+        label="Incrementar!!"
+      />
     </View>
   );
 };
@@ -29,14 +26,5 @@ const styles = StyleSheet.create({
     fontSize: 80,
     color: 'black',
     fontWeight: '300',
-  },
-  button: {
-    backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 10,
-  },
-  buttonPressed: {
-    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
   },
 });
