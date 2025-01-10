14183c50a937e50c3d72791c7229ed87eb93fd07
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-10 10:28:28 -0600

    Instalar iconos en React Native - Android using Ionicons

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -3,11 +3,14 @@ import {Text, View} from 'react-native';
 import {globalStyles} from '../theme/global.styles';
 import {FAB} from 'react-native-paper';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export const CounterM3Screen = () => {
   const [count, setCount] = useState(1);
   return (
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
+      <Icon name="accessibility-outline" size={25} />
       <FAB
         label="+1"
         style={globalStyles.fab}
